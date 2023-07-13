5babe821acc5679b6c9d3d9b7b92c6dd52ad2035
MosNes <7hekarl@gmail.com>
2023-07-13 15:44:05 -0400

    debugged and fixed broken navbar icon

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -2,15 +2,36 @@
 
         <v-app-bar :elevation="2" app color="primary">
 
-            <v-app-bar-nav-icon variant="text" color="secondary" @click.stop="drawer = !drawer" />
+            <v-app-bar-nav-icon variant="text" color="secondary" @click.stop="data.drawer = !data.drawer" />
                 <v-app-bar-title>DELVERS CHARACTER GENERATOR</v-app-bar-title>
                 <v-spacer></v-spacer>
                 <v-btn text>Home</v-btn>
 
         </v-app-bar>
 
+        <v-navigation-drawer v-model="data.drawer" app class="bg-info">
+            <v-list>
+                <v-list-item v-for="link in data.links" key="link.name" router :to="link.link">
+                    <v-list-item-title>
+                        {{ link.name }}
+                    </v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
 </template>
 
 <script setup>
+
+    import { reactive } from 'vue';
+
+    const data = reactive({
+
+        drawer: false,
+        links: [
+            { name: 'Home', icon: 'mdi-home', link: '/' },
+        ]
+
+    })
 
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 //vuetify
+//need to install mdi package and import this css file to use mdi Icons with vuetify
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -18,6 +20,7 @@ const vuetify = createVuetify({
             mdi,
         }
     },
+    // set up custom theme colors
     theme: {
         defaultTheme: 'myTheme',
         themes: {
