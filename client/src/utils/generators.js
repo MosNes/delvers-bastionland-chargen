69671ab7e827970a6eb6015ad6ec55f3934c9a13
MosNes <7hekarl@gmail.com>
2023-07-13 10:02:46 -0400

    added generateSpecies() for testing

--- client/src/utils/generators.js
+++ client/src/utils/generators.js
@@ -1,4 +1,14 @@
 import {DiceRoll} from '@dice-roller/rpg-dice-roller';
+import { speciesTable, descriptorsTable } from './randomTables';
 
+export const generateSpecies = () => {
 
-export const generateSpecies = () => {};
+    //generate die size based on number of species in table
+    const dieSize = descriptorsTable.length;
+
+    //roll a die with the number of sides equal to the number of species in the table
+    const roll = new DiceRoll(`1d${dieSize}`);
+
+    //return the species at the index of the roll result
+    return speciesTable[roll.total - 1];
+};
