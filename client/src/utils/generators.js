0294f50317375a502df16ce6364ac3bf58da0a6a
MosNes <7hekarl@gmail.com>
2023-07-13 08:32:07 -0400

    added randomtables.js and species table

---
+++ client/src/utils/generators.js
@@ -0,0 +1,4 @@
+import {DiceRoll} from '@dice-roller/rpg-dice-roller';
+
+
+export const generateSpecies = () => {};
---
+++ client/src/utils/randomTables.js
@@ -0,0 +1,64 @@
+const speciesTable = [
+    {
+        name: 'Canid',
+        description: 'Dogs, Wolves, Foxes, Jackals, Coyotes, etc.'
+    },
+    {
+        name: 'Felid',
+        description: 'Cats, Lions, Tigers, Leopards, etc.'
+    },
+    {
+        name: 'Ursid',
+        description: 'Bears, Pandas, etc.'
+    },
+    {
+        name:'Avian',
+        description: 'Songbirds, Waterfowl, Raptors, etc'
+    },
+    {
+        name: 'Mousekin',
+        description: 'Mice, Rats, small rodents, etc.'
+    },
+    {
+        name: 'Lapine',
+        description: 'Rabbits and Hares'
+    },
+    {
+        name: 'Badgerkin',
+        description: 'Badgers, Raccoons, Weasels, Ferrets, Otters, etc.'
+    },
+    {
+        name:  'Lizardkin',
+        description: 'Lizards, Snakes, Crocodiles, Turtles etc.'
+    },
+    {
+        name: 'Xolotl',
+        description: 'Salamanders, Frogs, Toads, etc'
+    },
+    {
+        name: 'Chiroptid',
+        description: 'Bats'
+    },
+    {
+        name: 'Pilosan',
+        description: 'Sloths, Anteaters, Pangolins, etc.'
+    },
+    {
+        name: 'Loxodon',
+        description: 'Elephants, Mammoths'
+    },
+    {
+        name: 'Clovenhoof',
+        description: 'Goats, Sheep, Deer, Cows, Pigs, Buffalo, Giraffes etc.'
+    },
+    {
+        name: 'Solidhoof',
+        description: 'Horses, Zebras, Rhinos, etc.'
+    },
+    {
+        name: 'Pouchborn',
+        description: 'Opossums, Kangaroos, Wombats, Koalas, Platypi, etc.'
+    }
+];
+
+ 
